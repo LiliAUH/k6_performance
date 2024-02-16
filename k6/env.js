ccdbff1aa0b8a05a93df8ab69ff76a02ebfd7b04
LiliAUH <aline.hatakeyama@gmail.com>
2024-02-16 14:59:59 -0300

    Adding more exercise for load test

---
+++ k6/env.js
@@ -0,0 +1,12 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+
+export default function(){
+
+    const baseUrl = __ENV.URL;
+
+    const res = http.get(__ENV.URL);
+
+    sleep(1);
+}
